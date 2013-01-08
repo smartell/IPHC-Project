16920a0c620e58711699b859218a3faa8dba72f6
Steve Martell <martell.steve@gmail.com>
2013-01-08 06:09:05 -0800

    Using logit transformation for se %*% P and Ro as leading

--- HTM/DDModel.R
+++ HTM/DDModel.R
@@ -22,7 +22,7 @@ wk      <- alpha * (1-rho^agek)/(1-rho)
 # |----------------------------------------------------------------------------|
 # | Movement matrix based on gravity model
 # |----------------------------------------------------------------------------|
-R       <- 5.5
+R       <- 1.5
 g       <- rnorm(A,0,1.0)
 P       <- matrix(exp(g),nrow=A,ncol=A,byrow=FALSE)
 diag(P) <- diag(P) + exp(R)
@@ -38,23 +38,24 @@ apportion <- runif(A,0.0,1.0)
 apportion <- apportion/sum(apportion)
 wbar      <- (s*alpha+ wk*(1-s)) / (1-rho*s)
 ro        <- bo/wbar*(1-s)
-be        <- bo * apportion
+# be        <- bo * apportion
 re        <- ro * apportion 
-se        <- rep(s,A)
-we        <- ((se*alpha)%*%P + wk*(1-se%*%P))/(1-(se*rho)%*%P)
-we        <- -(se%*%P*alpha - wk*(se%*%P) + wk)/(-1 + (se%*%P)*rho)
-be        <- wk*re/(1.-se%*%P*(alpha/we+rho))
-be        <- -wk*re*we/(alpha*se%*%P+rho*we*se%*%P-we)
+se        <- exp(-rep(m,A) %*% P)/(1+exp(-rep(m,A) %*% P))
+we        <- ((se*alpha) + wk*(1-se))/(1-(se*rho))
+we        <- -(se*alpha - wk*(se) + wk)/(-1 + (se)*rho)
+# be        <- wk*re/(1.-se*(alpha/we+rho))
+be        <- -wk*re*we/(alpha*se+rho*we*se-we)
+bo        <- sum(be)
 ne        <- be / we
 print(ne)
-# re        <- (be - se*be*(alpha/we+rho)%*%P)/wk
+# re        <- (be - se*be*(alpha/we+rho))/wk
 
-# ne        <- (be*(-1+se)%*%P)/(we*(se%*%P-1))
+ne        <- (be*(-1+se))/(we*(se-1))
 # The following is the same as ne=be/we
-ne        <- ((be-wk*re)%*%solve(P)/se - rho*be)/alpha
+#ne        <- ((be-wk*re)%*%solve(P)/se - rho*be)/alpha
 print(ne)
-# Constraint in that ne >= se*ne%*%P
-# pe        <- ((ne - se*ne%*%P)*wbar)/(bo*(1.-s))
+# Constraint in that ne >= se*ne
+# pe        <- ((ne - se*ne)*wbar)/(bo*(1.-s))
 pe        <- re/sum(re)
 so        <- reck * (ro/bo)
 beta      <- (reck-1.0)/bo
@@ -82,7 +83,7 @@ for(i in 1:T)
 		rt[i,] = pe*(so*st/(1.+beta*st))
 	}
 
-	bt[i+1,] <- (se%*%P)*(alpha*nt[i,]+rho*bt[i,]) + wk*rt[i,]
-	nt[i+1,] <- (se%*%P)*nt[i,] + rt[i,]
+	bt[i+1,] <- (se)*(alpha*nt[i,]+rho*bt[i,]) + wk*rt[i,]
+	nt[i+1,] <- (se)*nt[i,] + rt[i,]
 	wt[i+1,] <- bt[i+1,]/nt[i+1,]
 }
