7830d2c029e0d1e162405613514521aa3110bcfb
Steve Martell <martell.steve@gmail.com>
2013-01-08 08:25:41 -0800

    Now have model at equilibrium using Bo as leading parameter

--- HTM/DDModel.R
+++ HTM/DDModel.R
@@ -1,6 +1,7 @@
 # |---------------------------------------------------------------------------|
 # | Delay Difference Model
 # |---------------------------------------------------------------------------|
+setwd("/Users/stevenmartell1/Documents/IPHC/IPHC-project/HTM/")
 set.seed(999)
 
 
@@ -22,11 +23,11 @@ wk      <- alpha * (1-rho^agek)/(1-rho)
 # |----------------------------------------------------------------------------|
 # | Movement matrix based on gravity model
 # |----------------------------------------------------------------------------|
-R       <- 1.5
+R       <- 50.5
 g       <- rnorm(A,0,1.0)
-P       <- matrix(exp(g),nrow=A,ncol=A,byrow=FALSE)
-diag(P) <- diag(P) + exp(R)
-P       <- P/rowSums(P)
+P       <- matrix((g),nrow=A,ncol=A,byrow=FALSE)
+diag(P) <- diag(P) + (R)
+P       <- t(P)/rowSums(P)
 # P       <- diag(1,A)
 
 
@@ -36,29 +37,38 @@ P       <- P/rowSums(P)
 # |----------------------------------------------------------------------------|
 apportion <- runif(A,0.0,1.0)
 apportion <- apportion/sum(apportion)
-wbar      <- (s*alpha+ wk*(1-s)) / (1-rho*s)
-ro        <- bo/wbar*(1-s)
-# be        <- bo * apportion
-re        <- ro * apportion 
-se        <- exp(-rep(m,A) %*% P)/(1+exp(-rep(m,A) %*% P))
-we        <- ((se*alpha) + wk*(1-se))/(1-(se*rho))
-we        <- -(se*alpha - wk*(se) + wk)/(-1 + (se)*rho)
-# be        <- wk*re/(1.-se*(alpha/we+rho))
-be        <- -wk*re*we/(alpha*se+rho*we*se-we)
-bo        <- sum(be)
-ne        <- be / we
-print(ne)
-# re        <- (be - se*be*(alpha/we+rho))/wk
 
-ne        <- (be*(-1+se))/(we*(se-1))
-# The following is the same as ne=be/we
-#ne        <- ((be-wk*re)%*%solve(P)/se - rho*be)/alpha
-print(ne)
-# Constraint in that ne >= se*ne
-# pe        <- ((ne - se*ne)*wbar)/(bo*(1.-s))
+be        <- bo * apportion
+se        <- exp(rep(s,A) %*% P)/(1+exp(rep(s,A) %*% P))
+we        <- -(se*alpha - wk*(se) + wk)/(-1 + (se)*rho)
+re        <- -be*(se*alpha+se*rho*we-we)/(we*wk)
+ne        <- be / we
+ro        <- sum(re)
+
 pe        <- re/sum(re)
 so        <- reck * (ro/bo)
 beta      <- (reck-1.0)/bo
+
+# wbar      <- (s*alpha+ wk*(1-s)) / (1-rho*s)
+# ro        <- bo/wbar*(1-s)
+# be        <- bo * apportion
+# re        <- ro * apportion 
+# se        <- exp(-rep(m,A) %*% P)/(1+exp(-rep(m,A) %*% P))
+# we        <- ((se*alpha) + wk*(1-se))/(1-(se*rho))
+
+# be        <- wk*re/(1.-se*(alpha/we+rho))
+# be        <- -wk*re*we/(alpha*se+rho*we*se-we)
+# bo        <- sum(be)
+
+# print(ne)
+# re        <- (be - se*be*(alpha/we+rho))/wk
+
+# ne        <- (be*(-1+se))/(we*(se-1))
+# The following is the same as ne=be/we
+#ne        <- ((be-wk*re)%*%solve(P)/se - rho*be)/alpha
+# print(ne)
+# Constraint in that ne >= se*ne
+# pe        <- ((ne - se*ne)*wbar)/(bo*(1.-s))
 
 
 # |----------------------------------------------------------------------------|
@@ -87,3 +97,5 @@ for(i in 1:T)
 	nt[i+1,] <- (se)*nt[i,] + rt[i,]
 	wt[i+1,] <- bt[i+1,]/nt[i+1,]
 }
+
+matplot(bt,type="l",las=1)
