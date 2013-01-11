491ecdcf4371284a06b95093fad4eaf35e99901a
Steve Martell <martell.steve@gmail.com>
2013-01-11 13:37:23 -0800

    Now have proper equilibrium model working with migration for DDmodel

--- HTM/DDModel.R
+++ HTM/DDModel.R
@@ -2,14 +2,14 @@
 # | Delay Difference Model
 # |---------------------------------------------------------------------------|
 setwd("/Users/stevenmartell1/Documents/IPHC/IPHC-project/HTM/")
-set.seed(999)
+set.seed(939)
 
 
 
 # |---------------------------------------------------------------------------|
 # | Model parameters
 # |---------------------------------------------------------------------------|
-A       <- 4    				#Number of areas
+A       <- 8    				#Number of areas
 agek    <- 6
 bo      <- 100
 reck    <- 12
@@ -23,23 +23,24 @@ wk      <- alpha * (1-rho^agek)/(1-rho)
 # |----------------------------------------------------------------------------|
 # | Movement matrix based on gravity model
 # |----------------------------------------------------------------------------|
-R       <- 50.5
+R       <- 4.5
 g       <- rnorm(A,0,1.0)
-P       <- matrix((g),nrow=A,ncol=A,byrow=FALSE)
-diag(P) <- diag(P) + (R)
+P       <- matrix(exp(g),nrow=A,ncol=A,byrow=FALSE)
+diag(P) <- diag(P) + exp(R)
 P       <- t(P)/rowSums(P)
 # P       <- diag(1,A)
 
 
 
 # |----------------------------------------------------------------------------|
-# | Equilibrium conditions.
+# | Equilibrium unfished conditions.
 # |----------------------------------------------------------------------------|
 apportion <- runif(A,0.0,1.0)
 apportion <- apportion/sum(apportion)
 
 be        <- bo * apportion
-se        <- exp(rep(s,A) %*% P)/(1+exp(rep(s,A) %*% P))
+ze        <- rep(log(s/(1-s)),A)
+se        <- exp(ze %*% P)/(1+exp(ze %*% P))
 we        <- -(se*alpha - wk*(se) + wk)/(-1 + (se)*rho)
 re        <- -be*(se*alpha+se*rho*we-we)/(we*wk)
 ne        <- be / we
@@ -48,27 +49,39 @@ ro        <- sum(re)
 pe        <- re/sum(re)
 so        <- reck * (ro/bo)
 beta      <- (reck-1.0)/bo
+aj        <- reck * (re/be)
+bj        <- (reck-1.0)/be
 
-# wbar      <- (s*alpha+ wk*(1-s)) / (1-rho*s)
-# ro        <- bo/wbar*(1-s)
-# be        <- bo * apportion
-# re        <- ro * apportion 
-# se        <- exp(-rep(m,A) %*% P)/(1+exp(-rep(m,A) %*% P))
-# we        <- ((se*alpha) + wk*(1-se))/(1-(se*rho))
+# |----------------------------------------------------------------------------|
+# | Equilibrium fished conditions.
+# |----------------------------------------------------------------------------|
+# | Given a vector of fishing mortality rates calculate corresponding 
+# | equilibrium conditions, including yields (ye)
+ue <- rep(0.5,A)
+equil <- function(ue)
+{	
+	ue <- c(1,1,1,1,0.8,0.8,0.8,0.8)*ue
+	ze <- rep(log(s/(1-s)),A)
+	se <- exp(ze %*% P)/(1+exp(ze %*% P))*(1-ue)
+	we <- -(se*alpha - wk*(se) + wk)/(-1 + (se)*rho)
+	be <- -(-we+se*alpha+se*rho*we+wk*aj*we)/(bj*(-we+se*alpha+se*rho*we))
+	re <- aj*be/(1.0+bj*be)
+	for(i in 1:100)
+	{
+		re <- pe * (so*sum(be)/(1+beta*sum(be)))
+		be <- se * be *(alpha/we+rho)+ wk*re 
+	}
+	ne <- be / we
+	ye <- ue * be
+	return(ye)
+}
 
-# be        <- wk*re/(1.-se*(alpha/we+rho))
-# be        <- -wk*re*we/(alpha*se+rho*we*se-we)
-# bo        <- sum(be)
-
-# print(ne)
-# re        <- (be - se*be*(alpha/we+rho))/wk
-
-# ne        <- (be*(-1+se))/(we*(se-1))
-# The following is the same as ne=be/we
-#ne        <- ((be-wk*re)%*%solve(P)/se - rho*be)/alpha
-# print(ne)
-# Constraint in that ne >= se*ne
-# pe        <- ((ne - se*ne)*wbar)/(bo*(1.-s))
+YE <- function(log.ue)
+{
+	ue <- exp(log.ue)
+	ye<- equil(ue)
+	return(sum(ye))
+}
 
 
 # |----------------------------------------------------------------------------|
@@ -78,14 +91,14 @@ T  <- 100
 bt = nt = rt = wt = matrix(NA,nrow=T+1,ncol=A)
 bt[1,] = be
 nt[1,] = ne
-rt[1,] = pe*ro 
+rt[1,] = re #pe*ro 
 wt[1,] = be/ne
 for(i in 1:T)
 {
 
 	if(i <= agek)
 	{
-		rt[i,] = pe*ro 
+		rt[i,] = re#pe*ro 
 	}
 	else
 	{
@@ -99,3 +112,6 @@ for(i in 1:T)
 }
 
 matplot(bt,type="l",las=1)
+fe <- seq(0,0.5,by=0.02)
+ye <- t(sapply(fe,equil))
+matplot(fe,ye,type="o")
